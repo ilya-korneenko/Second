7366373e38df701dc22fd48d0e03224709de476e
ilya-korneenko <korneenko.2001@mail.ru>
2022-01-23 04:20:11 +0300

    proba3

--- src/by/proger/korneenko/jd01_06/TaskA2.java
+++ src/by/proger/korneenko/jd01_06/TaskA2.java
@@ -1,2 +1,45 @@
-package by.proger.korneenko.jd01_06;public class TaskA2 {
+package by.proger.korneenko.jd01_06;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskA2 {
+    private static String[] wordsArr =new String[0];//массив слов
+    private static int[] count ={}; //массив повторений
+
+    private static int pos(String word){
+        for (int i = 0; i < wordsArr.length; i++) {
+            if (wordsArr[i].equals(word))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[А-Яа-яёЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find())
+        {
+            String word = matcher.group();
+            int p = pos(word);
+            if (p>=0){
+                count[p]++;
+            }
+            else
+            {
+                int last = wordsArr.length;
+                wordsArr = Arrays.copyOf(wordsArr,last+1);
+                wordsArr[last]=word;
+                count=Arrays.copyOf(count,last+1);
+                count[last]=1;
+            }
+            for (int i = 0; i < wordsArr.length; i++) {
+                System.out.println(wordsArr[i]+"->"+count[i]);
+            }
+        }
+    }
 }
--- src/by/proger/korneenko/jd01_06/TaskB1.java
+++ src/by/proger/korneenko/jd01_06/TaskB1.java
@@ -1,2 +1,20 @@
-package by.proger.korneenko.jd01_06;public class TaskB1 {
+package by.proger.korneenko.jd01_06;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[\\p{Blank}\\p{Punct}|\\s|\\p{Space}|\\p{Blank}|\\p{Punct}|—]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        String str=Poem.text;
+        //String [] arrStr={};
+        String [] arrStr = str.split(pattern.pattern());
+//        while (matcher.find())
+//            System.out.println(matcher.group());
+        for (int i = 0; i < arrStr.length; i++) {
+            System.out.println(arrStr[i]);
+        }
+    }
 }
+//уеыаоэяиюёУЕЫАОЭЯИЮ
